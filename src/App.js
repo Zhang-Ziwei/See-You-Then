351dd8615dc9e929cc956b9bca3c174268b674ad
ohohmymygodgod <working.cby@gmail.com>
2022-11-14 14:15:00 +0800

    Finish Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import ResponsiveAppBar from "./component/ResponsiveAppBar";
+import HomePage from './component/HomePage';
 
 function App() {
+  const [state, setState] = useState(0);
   return (
-    <ResponsiveAppBar />
+    <div>
+      <ResponsiveAppBar />
+      {(state == 0)? <HomePage /> :
+       (state == 1)? <div></div> : <div></div>}
+    </div>
   );
 }
 
